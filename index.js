20826eecf2899d59c27b672f6c88d2077201b043
wallypark710 <wallypark710@gmail.com>
2020-01-08 20:40:09 +0900

    Add livecoding log

---
+++ index.js
@@ -0,0 +1,45 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+
+  type Mutation {
+    toggleName(name: String): Book
+  }
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+const resolvers = {
+  Query: {
+    books: () => books
+  },
+
+  Mutation: {
+    async toggleName(_, { name }) {
+      return { title: "success" };
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
